b837340be77d61372fa9169fe9cdf73a3c8c1d49
daidh1 <daidh@goline.vn>
2021-07-18 23:41:42 +0700

    update url map

--- src/app/shared/components/menus/menu-frame.ts
+++ src/app/shared/components/menus/menu-frame.ts
@@ -150,191 +150,191 @@ export const MenuMap = [
     {
         "state_code": "haiphong",
         "state_name": "Hải Phòng",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.haiphong.gov.vn'
     },
     {
         "state_code": "hatinh",
         "state_name": "Hà Tĩnh",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.hatinh.gov.vn'
     },
     {
         "state_code": "nghean",
         "state_name": "Nghệ An",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.nghean.gov.vn'
     },
     {
         "state_code": "quangbinh",
         "state_name": "Quảng Bình",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.quangbinh.gov.vn'
     },
     {
         "state_code": "quangtri",
         "state_name": "Quảng Trị",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.quangtri.gov.vn'
     },
     {
         "state_code": "thanhhoa",
         "state_name": "Thanh Hóa",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.thanhhoa.gov.vn'
     },
     {
         "state_code": "thuathienhue",
         "state_name": "Thừa Thiên–Huế",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.thuathienhue.gov.vn'
     },
     {
         "state_code": "daklak",
         "state_name": "Đắk Lắk",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.daklak.gov.vn'
     },
     {
         "state_code": "daknong",
         "state_name": "Đắk Nông",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.daknong.gov.vn'
     },
     {
         "state_code": "gialai",
         "state_name": "Gia Lai",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.gialai.gov.vn'
     },
     {
         "state_code": "kontum",
         "state_name": "Kon Tum",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.kontum.gov.vn'
     },
     {
         "state_code": "lamdong",
         "state_name": "Lâm Đồng",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.lamdong.gov.vn'
     },
     {
         "state_code": "binhdinh",
         "state_name": "Bình Định",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.binhdinh.gov.vn'
     },
     {
         "state_code": "binhthuan",
         "state_name": "Bình Thuận",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.binhthuan.gov.vn'
     },
     {
         "state_code": "khanhhoa",
         "state_name": "Khánh Hòa",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.khanhhoa.gov.vn'
     },
     {
         "state_code": "ninhthuan",
         "state_name": "Ninh Thuận",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.ninhthuan.gov.vn'
     },
     {
         "state_code": "phuyen",
         "state_name": "Phú Yên",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.phuyen.gov.vn'
     },
     {
         "state_code": "quangnam",
         "state_name": "Quảng Nam",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.quangnam.gov.vn'
     },
     {
         "state_code": "quangngai",
         "state_name": "Quảng Ngãi",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.quangngai.gov.vn'
     },
     {
         "state_code": "danang",
         "state_name": "Đà Nẵng",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.danang.gov.vn'
     },
     {
         "state_code": "bariavungtau",
         "state_name": "Bà Rịa–Vũng Tàu",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.bariavungtau.gov.vn'
     },
     {
         "state_code": "binhduong",
         "state_name": "Bình Dương",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.binhduong.gov.vn'
     },
     {
         "state_code": "binhphuoc",
         "state_name": "Bình Phước",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.binhphuoc.gov.vn'
     },
     {
         "state_code": "dongnai",
         "state_name": "Đồng Nai",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.dongnai.gov.vn'
     },
     {
         "state_code": "tayninh",
         "state_name": "Tây Ninh",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.tayninh.gov.vn'
     },
     {
         "state_code": "angiang",
         "state_name": "An Giang",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.angiang.gov.vn'
     },
     {
         "state_code": "baclieu",
         "state_name": "Bạc Liêu",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.baclieu.gov.vn'
     },
     {
         "state_code": "bentre",
         "state_name": "Bến Tre",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.bentre.gov.vn'
     },
     {
         "state_code": "camau",
         "state_name": "Cà Mau",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.camau.gov.vn'
     },
     {
         "state_code": "dongthap",
         "state_name": "Đồng Tháp",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.dongthap.gov.vn'
     },
     {
         "state_code": "haugiang",
         "state_name": "Hậu Giang",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.haugiang.gov.vn'
     },
     {
         "state_code": "kiengiang",
         "state_name": "Kiên Giang",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.kiengiang.gov.vn'
     },
     {
         "state_code": "longan",
         "state_name": "Long An",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.longan.gov.vn'
     },
     {
         "state_code": "soctrang",
         "state_name": "Sóc Trăng",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.soctrang.gov.vn'
     },
     {
         "state_code": "tiengiang",
         "state_name": "Tiền Giang",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.tiengiang.gov.vn'
     },
     {
         "state_code": "travinh",
         "state_name": "Trà Vinh",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.travinh.gov.vn'
     },
     {
         "state_code": "vinhlong",
         "state_name": "Vĩnh Long",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.vinhlong.gov.vn'
     },
     {
         "state_code": "cantho",
         "state_name": "Cần Thơ",
-        "state_iframe": 'https://covidmaps.bacgiang.gov.vn'
+        "state_iframe": 'https://covidmaps.cantho.gov.vn'
     }
 ]
